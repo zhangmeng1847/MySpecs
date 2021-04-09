a48a01fa0e728c14e30b428708f0040c39d17fe4
zhangmeng <zhangmeng1847@126.com>
2021-04-09 09:49:48 +0800

    第一次提交

---
+++ Classes/AFSecurityPolicy+HTTPS.h
@@ -0,0 +1,18 @@
+//
+//  AFSecurityPolicy+HTTPS.h
+//  WAKA
+//
+//  Created by dev on 2020/8/10.
+//  Copyright © 2020 CreditEase. All rights reserved.
+//
+
+#ifdef DEBUG
+#import "AFSecurityPolicy.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AFSecurityPolicy (HTTPS)
+@end
+
+NS_ASSUME_NONNULL_END
+#endif
